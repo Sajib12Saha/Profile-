8237c515f0c49a1c95abf305350c695db6bd9608
Sajib12Saha <145220509+Sajib12Saha@users.noreply.github.com>
2025-01-26 01:25:51 +0600

    Update profile.tsx

--- components/profile.tsx
+++ components/profile.tsx
@@ -41,7 +41,7 @@ export const Profile = () => {
                         src="/images/sojib2.png"
                         alt="Sajib"
                         fill
-                        className="shadow-md rounded-full object-cover" 
+                        className="shadow-md rounded-md object-cover" 
                     />
                 </div>
                 <div className="flex flex-col">
